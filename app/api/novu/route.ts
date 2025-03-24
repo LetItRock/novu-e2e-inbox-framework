d09c1ddbbc9e8aadef3825f141ab901ab80c57d9
Paweł <pawel.tymczuk@gmail.com>
2025-03-24 12:19:50 +0100

    chore: update framework

--- app/api/novu/route.ts
+++ app/api/novu/route.ts
@@ -1,9 +1,9 @@
-import { serve } from '@novu/framework/next';
+import { serve } from "@novu/framework/next";
 import {
   welcomeOnboardingEmail,
   welcomeOnboardingEmail2,
   theNewInboxNotification,
-} from '../../novu/workflows';
+} from "../../novu/workflows";
 
 // the workflows collection can hold as many workflow definitions as you need
 export const { GET, POST, OPTIONS } = serve({
